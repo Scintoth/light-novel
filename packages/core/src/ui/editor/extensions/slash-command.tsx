67cbaaa86571d91c937e5acd4979f00443864713
Scintoth <Scott93Davis@gmail.com>
2023-11-07 01:52:37 +0000

    Removed AI completion

--- packages/core/src/ui/editor/extensions/slash-command.tsx
+++ packages/core/src/ui/editor/extensions/slash-command.tsx
@@ -77,12 +77,6 @@ const Command = Extension.create({
 const getSuggestionItems = ({ query }: { query: string }) => {
   return [
     {
-      title: "Continue writing",
-      description: "Use AI to expand your thoughts.",
-      searchTerms: ["gpt"],
-      icon: <Magic className="novel-w-7" />,
-    },
-    {
       title: "Send Feedback",
       description: "Let us know how we can improve.",
       icon: <MessageSquarePlus size={18} />,
